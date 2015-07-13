e7b365e100a834e174bc2da27932e2b6550f54bf
Randall Maas <randym@randym.name>
2015-07-12 19:42:36 -0500

    Added the ability to refer to directions in the abstract, and uses the graph representation to represent the topology

--- ZStructuring/InStory-Dir.cs
+++ ZStructuring/InStory-Dir.cs
@@ -7,59 +7,156 @@ partial class InStory
    /// <summary>
    /// This is the kind that is the base kind for "element of space"
    /// </summary>
-   static ZRoot roomKind = new ZObject("room");
+   internal static ZObject roomKind = new ZObject("room");
 
    /// <summary>
    /// This is the kind that is the base kind for direction
    /// </summary>
-   static ZRoot dirKind = new ZObject("direction");
-
-   static WordList east = "east easterly eastward eastwardly";
-   static WordList west = "west westward westwardly";
-   static WordList north= "north northerly northward northwardly";
-   static WordList south= "south southerly southward southwardly";
+   internal static ZObject dirKind = new ZObject("direction");
 
    /// <summary>
-   /// The various directions
+   /// A direction
    /// </summary>
-   static List<WordList> directions = new List<WordList>(){east, west, north,
-      south};
-
-   /// <summary>
-   /// See if the phrase matches a direction
-   /// </summary>
-   /// <param name="lexer"></param>
-   /// <returns></returns>
-   static WordList matchDirection(Lexer lexer)
+   /// <returns>The new object</returns>
+   ZObject Direction(WordList nouns)
    {
-      // See if the text directly refers to a relationship name (eg a direction)
-      //  a. Make a list of relationships
-      //  b. See which matches best
-
-      var orig = lexer.Save();
-      // Do a match on the directions keeping the best
-      WordList best=null;
-      var bestScore = 0.0;
-      var bestNumWords = 0;
-      var bestSave = orig;
-      foreach (var dir in directions)
-      {
-         lexer.Restore(orig);
-         var myNumWords=0;
-         // Get a score of how well it matches
-         var myScore = dir.matchMembers(lexer, out myNumWords);
-         if (myNumWords > bestNumWords || myScore > bestScore)
-         {
-            // Save the results
-            bestSave     = lexer.Save();
-            bestNumWords = myNumWords;
-            bestScore    = myScore;
-            best         = dir;
-         }
-      }
-      // Move past the word/phrase
-      lexer.Restore(bestSave);
-      // 
-      return best;
+      var ret= Kind((ZObject) Object(), dirKind);
+      ret.shortDescription=nouns.words[0];
+      ret.nouns = nouns;
+      return ret;
    }
+
+   /// <summary>
+   /// The different names for east
+   /// </summary>
+   static WordList eastWords="east easterly eastward eastwardly";
+   /// <summary>
+   /// The different names for west
+   /// </summary>
+   static WordList westWords = "west westward westwardly";
+   /// <summary>
+   /// The different names for north
+   /// </summary>
+   static WordList northWords= "north northerly northward northwardly";
+   /// <summary>
+   /// The different names for south
+   /// </summary>
+   static WordList southWords= "south southerly southward southwardly";
+
+   /// <summary>
+   /// The representation of the east direction
+   /// </summary>
+   internal ZObject east;
+   /// <summary>
+   /// The representation of the west direction
+   /// </summary>
+   internal ZObject west;
+   /// <summary>
+   /// The representation of the north direction
+   /// </summary>
+   internal ZObject north;
+   /// <summary>
+   /// The representation of the south direction
+   /// </summary>
+   internal ZObject south;
+
+   /// <summary>
+   /// A delegate to that adds the relation between the two objects
+   /// </summary>
+   /// <param name="source">The source party in the relation</param>
+   /// <param name="sink">The sink party in the relation</param>
+   /// <description>
+   /// See directed, and transitive
+   /// </description>
+   delegate void dRelation2Add(ZObject source, ZObject sink);
+
+   /// <summary>
+   /// Maps the name of the relation to the handler to add it
+   /// </summary>
+   Dictionary<WordList,dRelation2Add> relation2Add =
+      new Dictionary<WordList,dRelation2Add>();
+
+#if false
+   void define(WordList relationName)
+   {
+      // Get the relation for the name; if none exists, add the structure
+      Dictionary<ZObject,ZEdge> relation;
+      if (!relation2.TryGetValue(relationName, out relation))
+      {
+         // Define a structure to hold the relation
+         relation2[relationName] = relation = new Dictionary<ZObject,ZEdge>();
+      }
+
+      // Create a delegate to add the items in directed
+      relation2Add[relationName] =  delegate(ZObject source, ZObject sink)
+      {
+         ZEdge edge;
+         // Add the entry to the table.. if none exists.
+         if (!relation.TryGetValue(source, out edge))
+         {
+            // Doesn't exist, create the relation
+            relation[source] = new ZEdge(newName(), sink, null);
+            return;
+         }
+         // Check that the relation is sensible
+         if (edge.sink == sink)
+            return;
+         // Oh-oh, there is a problem.
+      };
+   }
+#endif
+   /// <summary>
+   /// Adds the pair to the relation
+   /// </summary>
+   /// <param name="relationName"></param>
+   /// <param name="a"></param>
+   /// <param name="b"></param>
+   void add(WordList relationName, ZObject a, ZObject b)
+   {
+      relation2Add[relationName](a,b);
+   }
+
+   /// <summary>
+   /// An object to track it's opposite
+   /// </summary>
+   internal static ZObject opposite = new ZObject("opposite");
+   internal static ZObject _instance = new ZObject("instance");
+
+   /// <summary>
+   /// Create the relations
+   /// </summary>
+   void initRelations()
+   {
+      // This tracks which entities are "concrete" instances rather than abstract kinds
+      relations[_instance] = new Dictionary<ZObject, ZObject>();
+      addRelation2(__kind);
+      addRelation2(opposite);
+
+
+      // Define the directions
+      east = Direction(eastWords);
+      west = Direction(westWords);
+      north = Direction(northWords);
+      south = Direction(southWords);
+      ((SparseGraph<ZObject>)relations[opposite]).After(east, new Edge<ZObject>(west, null));
+      ((SparseGraph<ZObject>)relations[opposite]).After(west, new Edge<ZObject>(east, null));
+      ((SparseGraph<ZObject>)relations[opposite]).After(north, new Edge<ZObject>(south, null));
+      ((SparseGraph<ZObject>)relations[opposite]).After(south, new Edge<ZObject>(north, null));
+
+      // Define the relations for each direction
+      addRelation2(east);
+      addRelation2(west);
+      addRelation2(north);
+      addRelation2(south);
+   }
+   /// <summary>
+   /// Creates the underlying structures for the relation
+   /// </summary>
+   /// <param name="name">The relation to create</param>
+   void addRelation2(ZObject name)
+   {
+      if (!relations.ContainsKey(name))
+         relations[ name] =  new SparseGraph<ZObject>(true);
+   }
+
 }
--- ZStructuring/ZEdge.cs
+++ ZStructuring/ZEdge.cs
@@ -2,27 +2,68 @@
 using System.Collections.Generic;
 using System.Text;
 
-
-partial class ZEdge:ZRoot
+/// <summary>
+/// Represents a directed link to a node
+/// </summary>
+partial class InStory
 {
    /// <summary>
-   /// This is the (shared) object that may regulate passage,
-   /// or has properties for the edge
+   /// Checks to see if the gate if open or not
    /// </summary>
-   internal ZObject door;
-
-   /// <summary>
-   /// This is the other side of the edge
-   /// </summary>
-   internal ZCell sink;
-
-   internal ZEdge(string name, ZCell sink, ZObject door)
-      :base(name)
+   /// <param name="gate"></param>
+   /// <returns></returns>
+   bool isOpen(ZObject gate)
    {
-      this.sink = sink;
-      this.door = door;
+      return true;
    }
 
+   /// <summary>
+   /// Checks to see if we can go thru the edge/into the room
+   /// </summary>
+   /// <returns>true if we can traverse into it, false otherwise</returns>
+   internal bool canTraverseTo(Edge<ZObject> edge)
+   {
+      if (null == edge.gate || isOpen(edge.gate))
+         return true;
+      return false;
+   }
+
+#if false
+   /// <summary>
+   /// Finds the object that best matches the phrase
+   /// </summary>
+   /// <param name="phrase">The words to match</param>
+   /// <param name="index">The first word in phrase to match</param>
+   /// <param name="score">The score for the best match: 0 if no match[out]</param>
+   /// <param name="numWordsMatch">The number of words that match [out]</param>
+   /// <param name="bestMatch">The object that best matches [out]</param>
+   /// <param name="seen">Set of objects already examined</param>
+   public void match(Lexer lexer, ref double score,
+               ref int numWordsMatch,
+               ref object bestMatch,
+               ref LexState lexerState, Dictionary<object,object> seen)
+   {
+      // Skip if we've already seen this object
+      if (seen.ContainsKey(this))
+         return;
+      // Add ourself to prevent cycles
+      seen[this] = this;
+//      edge.match(lexer, ref score, ref numWordsMatch, ref bestMatch, ref lexerState, seen);
+#if true
+      /// We only check to see if the gate matches the phrase
+      if (null != edge.gate)
+         edge.gate.match(lexer, ref score, ref numWordsMatch,
+                             ref bestMatch, ref lexerState, seen);
+      // Should only match if no door, or door is open
+      if (null != edge.sink && (null == edge.gate || isOpen(edge.gate)))
+      {
+         // we only want match -- matchInContext checks the stuff from the parents..
+         edge.sink.match(lexer, ref score, ref numWordsMatch,
+                     ref bestMatch, ref lexerState, seen);
+      }
+#endif
+   }
+#endif
 
    /// <summary>
    /// Finds the object that best matches the phrase
@@ -33,29 +74,22 @@ partial class ZEdge:ZRoot
    /// <param name="numWordsMatch">The number of words that match [out]</param>
    /// <param name="bestMatch">The object that best matches [out]</param>
    /// <param name="seen">Set of objects already examined</param>
-   internal void matchInContext(Lexer lexer, ref double score,
+   internal static void match(Edge<ZObject> edge, Lexer lexer, ref double score,
                ref int numWordsMatch,
-               ref ZObject bestMatch,
-               ref LexState lexerState, Dictionary<ZRoot,object> seen)
+               ref object bestMatch,
+               ref LexState lexerState, Dictionary<object,object> seen)
    {
-      // Skip if we've already seen this object
-      if (seen.ContainsKey(this))
-         return;
-      // Add ourself to prevent cycles
-      seen[this] = this;
-
-      /// We only check to see if the door matches the phrase
-      if (null != door)
-         door.matchInContext(lexer, ref score, ref numWordsMatch,
+      /// We only check to see if the gate matches the phrase
+      if (null != edge.gate)
+         edge.gate.match(lexer, ref score, ref numWordsMatch,
                              ref bestMatch, ref lexerState, seen);
-      else
+      // Should only match if no door, or door is open
+      if (null != edge.sink && (null == edge.gate /*|| isOpen(gate)*/))
       {
-         // Should only match if no door, or door is open
-         if (null != sink )
-            // we only want match -- matchInContext checks the stuff from the parents..
-            sink.match(lexer, ref score, ref numWordsMatch,
-                             ref bestMatch, ref lexerState, seen);
+         // we only want match -- matchInContext checks the stuff from the parents..
+         edge.sink.match(lexer, ref score, ref numWordsMatch,
+                     ref bestMatch, ref lexerState, seen);
       }
-
    }
+
 }
